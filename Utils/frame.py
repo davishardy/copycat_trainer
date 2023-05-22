841d5cc53e9c320e2b64852f9d6572334bfffd48
davishardy <davischardy@gmail.com>
2023-05-22 17:06:18 -0400

    test commit

---
+++ Utils/frame.py
@@ -0,0 +1,4 @@
+# Utility to find all the frames within a folder with the specific frames
+
+def check_res(path):
+    pass
---
+++ main.py
@@ -0,0 +1,27 @@
+# The main script that enables cli / gui and usage
+
+# import nuke
+import sys
+import os
+import platform
+
+def main():
+    # Check the OS and assign a path to the application
+
+    cur_os = platform.system()
+    machine_id = platform.node()
+    
+
+    if "nb-r" or "mt-r" in machine_id:
+        if cur_os == "Darwin":
+            pass #Mac
+        elif cur_os == "Linux":
+            pass #Linux
+        else:
+            pass # windows
+    else:
+        # Running on a non-monty computer
+        pass
+
+
+main()
