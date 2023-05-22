7a30d1763f0077baa0224102f5260661722c4b47
davishardy <davischardy@gmail.com>
2023-05-22 19:22:26 -0400

    added first bit of nuke code

--- Utils/frame.py
+++ Utils/frame.py
@@ -1,4 +1,15 @@
 # Utility to find all the frames within a folder with the specific frames
+import nuke
 
-def check_res(path):
-    pass
+cur_node = nuke.toNode("Read1")
+
+widths = []
+heights = []
+
+for frame in range(1,20):
+    a = cur_node.metadata("input/width", frame)
+    b = cur_node.metadata("input/height", frame)
+
+
+c = nuke.filename(cur_node)
+
