69a0f2325f6d0f2772b034c5c02935712afaea7d
davishardy <davischardy@gmail.com>
2023-05-28 18:56:57 -0400

    node network setup and knob setup

--- Utils/node_graph.py
+++ Utils/node_graph.py
@@ -1,7 +1,8 @@
 import nuke
+import os
 
-GT_file = ""
-Input_file = ""
+gt_file = "/Users/davishardy/Downloads/sys_rf_arnold/output.0001-5.exr"
+input_file = "/Users/davishardy/Downloads/sys_rf_arnold/output.0001-5.exr"
 
 # check if file path exists
 # get padding
@@ -10,5 +11,13 @@ Input_file = ""
 # check is sequence is complete
 # Create nodes and link them together
 
+# Create nodes
+gt_read = nuke.nodes.Read(name="Ground_Truth", file= gt_file)
+input_read = nuke.nodes.Read(name="Input", file= input_file)
+cc_trainer = nuke.nodes.CopyCat(name="Train", inputs= [input_read, gt_read])
 
-ground_truth = nuke.nodes.Read(name="Ground_Truth", GT_file)
+
+cc_trainer["useGPUIfAvailable"].setValue("true")
+cc_trainer["epochs"].setValue(100)
+cc_trainer["imageInterval"].setValue(200)
+cc_trainer["isCachingEnabled"].setValue("false")
