2ef3becc9ecc956b045c27cab47471c409aadc57
davishardy <davischardy@gmail.com>
2023-05-28 02:31:11 -0400

    Working on setup

--- Utils/frame.py
+++ Utils/frame.py
@@ -1,15 +1,30 @@
 # Utility to find all the frames within a folder with the specific frames
 import nuke
 
-cur_node = nuke.toNode("Read1")
+def validate_inputs(nodes, start_frame, end_frame):
+    widths = []
+    heights = []
 
-widths = []
-heights = []
+    for node in nodes:
+        for frame in range(start_frame, end_frame):
+            width = nuke.toNode(node).metadata("input/width", frame)
+            height = nuke.toNode(node).metadata("input/height", frame)
 
-for frame in range(1,20):
-    a = cur_node.metadata("input/width", frame)
-    b = cur_node.metadata("input/height", frame)
+            try:
+                type(width) and type(height) == "int"
+            except ValueError:
+                print("Height and Width of frames weren't found")
+            else:
+                widths.append(width)
+                heights.append(height)
+
+    widths = set(widths)
+    heights = set(heights)
+
+    if len(widths) and len(heights) == 1:
+        return True
+    else:
+        return False
 
 
-c = nuke.filename(cur_node)
-
+# a = validate_inputs(["Read1", "Read2"], 0, 5)
---
+++ Utils/node_graph.py
@@ -0,0 +1,14 @@
+import nuke
+
+GT_file = ""
+Input_file = ""
+
+# check if file path exists
+# get padding
+# get begining and end
+# check if both sequences begining and ends are the same
+# check is sequence is complete
+# Create nodes and link them together
+
+
+ground_truth = nuke.nodes.Read(name="Ground_Truth", GT_file)
