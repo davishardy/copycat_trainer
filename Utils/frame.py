e9484530b69bbc182bb76e9ab857326b909a4007
davishardy <davischardy@gmail.com>
2023-05-31 14:15:29 -0400

    Finish validation functions

--- Utils/frame.py
+++ Utils/frame.py
@@ -1,30 +1,74 @@
-# Utility to find all the frames within a folder with the specific frames
-import nuke
+# Funtions to validate that sequence are the same size and shape
+# Created by Davis Hardy
+# Created on 2023-5-16
+# Version 1.1.0
 
-def validate_inputs(nodes, start_frame, end_frame):
+# Import modules
+import nuke
+import os
+import glob
+os.environ["OPENCV_IO_ENABLE_OPENEXR"]="1"
+import cv2  # Has to be below the env variable so it loads with that the EXR capabilities
+
+
+def file_elements(full_path):
+    """
+    Extracts file name elements from the full path of a file
+    Inputs: Full file path
+    Outputs: tuple of file name elements
+    """
+    try:
+        os.path.exists(full_path)
+
+    except ValueError:
+        print("file doesn't exist")
+
+    else:
+        base_name = os.path.basename(full_path)
+        base_name_elements = base_name.split(".")
+
+        try:
+            len(base_name_elements) == 3
+
+        except ValueError:
+            return None
+
+        else:
+            return (base_name_elements[0], base_name_elements[1], base_name_elements[2])
+
+
+def validate_inputs(paths):
+    """
+    Validates if all the frames are the same size
+    Inputs: Paths to start frames of both sequences
+    Outputs: Boolean
+    """
+
     widths = []
     heights = []
 
-    for node in nodes:
-        for frame in range(start_frame, end_frame):
-            width = nuke.toNode(node).metadata("input/width", frame)
-            height = nuke.toNode(node).metadata("input/height", frame)
+    frames_in_path = []
 
-            try:
-                type(width) and type(height) == "int"
-            except ValueError:
-                print("Height and Width of frames weren't found")
-            else:
-                widths.append(width)
-                heights.append(height)
+    for path in paths:
+        path_dir = os.path.dirname()
+        path_elements = file_elements(path)
+        included_frames = glob.glob(os.path.join(path_dir, path_elements[0], "*"))
 
+        frames_in_path.append(len(included_frames))
+
+        for frame in included_frames:
+            cur_frame = cv2.imread(frame, cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)  # Allows for reading of EXR's
+            dimensions = cur_frame.shape
+
+            widths.append(dimensions[1])
+            heights.append(dimensions[0])
+    
+    # Creates sets to get rid of duplicate outputs
     widths = set(widths)
     heights = set(heights)
-
-    if len(widths) and len(heights) == 1:
+    frame_quantity = set(frames_in_path)
+    
+    if len(widths) and len(heights) and len(frame_quantity) == 1:
         return True
     else:
         return False
-
-
-# a = validate_inputs(["Read1", "Read2"], 0, 5)
